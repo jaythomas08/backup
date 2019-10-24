76d37f5a6a067cb347446f797f7747424e8423d1
Jarrett A Thomas <56047051+jaythomas08@users.noreply.github.com>
2019-10-24 13:02:13 -0700

    Add files via upload

--- hangman.py
+++ hangman.py
@@ -3,26 +3,38 @@
 # October 21st, 2019
 # Hangman project
 
-secretWord = "arizona cardinals"
-secretWord = list(secretWord)
+print("Hello, and welcome to hangman!")
+numMisses = int(input("please type in number of misses you are allowed: "))
+
+secretWord = list("verrado")
+guessList = list("_______")
 
 guessList=[]
 for a in secretWord:
 	guessList.append("_")
 print(guessList)
 
-numMisses = int(input("please type in number of misses you are allowed: "))
+guess = input("guess a letter: ")
+index = 0
+for letter in secretWord:
+	if letter == guess:
+		guessList[index] = guess
+	index += 1 
+print(guessList)
 
-
+index = 0
 misses = 0
 while misses < numMisses:
 	guess = input("enter a letter: ")
 	if guess in secretWord:
-		print("letter is in word")
+		guessList[index] = guess
 	else:
 		misses += 1
 print(misses)
 
+count = 0
+for l in secretWord:
+	if l == letter:
+		print(count)
+		count += 1 
 
-
-print("GAME OVER BRUV BETTER LUCK NEXT TIME")
